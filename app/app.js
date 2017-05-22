bf2140ffb48d04ff24de13c6efc1540ba540198f
kjvelarde <karlvelarde@gmail.com>
2017-05-22 07:58:19 -0400

    Redefine Design and Coin Factory and dispenser

--- app/app.js
+++ app/app.js
@@ -12,71 +12,118 @@ app.factory('productFactory', function () {
                 Name: 'Cola',
                 Price: 1.00,
                 Quantity: 20,
-                Image: 'http://icons.iconarchive.com/icons/michael/coke-pepsi/512/Coca-Cola-Can-icon.png'
+                Image: '../assets/img/products/cola.jpg'
         },
             {
                 Name: 'Chips',
                 Price: .50,
                 Quantity: 20,
-                Image: 'http://images.clipartpanda.com/bag-of-potato-chips-clipart-PngMedium-potato-chips-bag-16629.png'
+                Image: '../assets/img/products/chips.jpg'
         },
             {
                 Name: 'Candy',
                 Price: .60,
                 Quantity: 2,
-                Image: 'http://iconshow.me/media/images/xmas/christmas-icon11/2/candy-512.png'
+                Image: '../assets/img/products/candy.jpg'
         },
             {
                 Name: 'Chocolate',
                 Price: 2.50,
                 Quantity: 0,
-                Image: 'https://cdn.iconscout.com/public/images/icon/free/png-512/chocolate-bar-candy-dairymilk-sweet-dessert-food-emoj-symbol-39cddddcf33bb40e-512x512.png'
+                Image: '../assets/img/products/chocolate.jpg'
         },
             {
                 Name: 'RedBull',
                 Price: 3.00,
                 Quantity: 0,
-                Image: 'http://www.zandh.co.uk/media/catalog/product/cache/1/thumbnail/600x600/9df78eab33525d08d6e5fb8d27136e95/r/e/red_bull.png'
+                Image: '../assets/img/products/redbull.jpg'
         }
-	];;
+	];
     pFactory.get = function () {
         return _products;
-    }
+    };
     return pFactory;
-})
+});
 
-app.controller('ctrl', function ($scope, $interval, $filter, productFactory) {
+app.factory('coinFactory', function () {
+    //src:https://www.usmint.gov/learn/coin-and-medal-programs/coin-specifications
+    var cFactory = {},
+        _coins = [
+            {
+                Name: 'Penny',
+                Value: .01,
+                WeightInGrams: 2.5,
+                Image: '../assets/img/penny.png',
+                Width: 18,
+                Height: 18
+            },
+            {
+                Name: 'Nickel',
+                Value: .05,
+                WeightInGrams: 5,
+                Image: '../assets/img/nickel.png',
+                Width: 20,
+                Height: 20
+            },
+            {
+                Name: 'Dime',
+                Value: .10,
+                WeightInGrams: 2.268,
+                Image: '../assets/img/dime.png',
+                Width: 15,
+                Height: 15
+            },
+            {
+                Name: 'Quarter',
+                Value: .25,
+                WeightInGrams: 5.670,
+                Image: '../assets/img/quarter.png',
+                Width: 30,
+                Height: 30
+            }
+
+        ];
+    cFactory.get = function () {
+        return _coins;
+    };
+    return cFactory;
+});
+
+app.controller('ctrl', function ($scope, $interval, $filter, productFactory, coinFactory) {
     $scope.statusMessage = 'Insert Coins';
     $scope.currentAmount = 0;
     $scope.changeAmount = 0;
-    $scope.selectedProduct = '';
+    $scope.changes = [];
+    $scope.selectedProducts = [];
     $scope.isExactChangeOnly = false;
     $scope.mode = 'normal';
 
     //Set of products inside the vending machine
     $scope.products = productFactory.get();
+    $scope.coins = coinFactory.get();
 
     //Return inserted coins
     $scope.returnCoins = function () {
         $scope.changeAmount += $scope.currentAmount;
         $scope.currentAmount = 0;
         $scope.statusMessage = $filter('currency')($scope.currentAmount, "$", 2)
-    }
+    };
 
     //Handle product vending/selection
     $scope.selectProduct = function (product) {
         if ($scope.currentAmount >= product.Price) {
             //Don't return 'change' if mode is 'Exact Change ONLY'
             if (!$scope.isExactChangeOnly) {
-                $scope.changeAmount += $scope.currentAmount - product.Price;
+                $scope.changeAmount += (($scope.currentAmount * 100 - product.Price * 100) / 100);
             }
+
             $scope.currentAmount = 0;
 
             $filter('filter')($scope.products, {
                 'Name': product.Name
             }).Quantity = product.Quantity--;
 
-            $scope.selectedProduct = product;
+            $scope.selectedProducts.push(angular.copy(product));
             $scope.statusMessage = 'THANK YOU'
         } else {
             $scope.statusMessage = 'PRICE ' + $filter('currency')(product.Price, "$", 2)
@@ -90,11 +137,16 @@ app.controller('ctrl', function ($scope, $interval, $filter, productFactory) {
     }
 
     //Handle Action when user insert a coin into the vending machine
-    $scope.insertCoin = function (val) {
-        if (val == .01) { // Reject pennies and put it directly to change amount
-            $scope.changeAmount += val;
+    $scope.insertCoin = function (coin) {
+        //Check weight of coin and determine the value
+        var val = $.grep($scope.coins, function (x) {
+            return x.WeightInGrams == coin.WeightInGrams;
+        });
+
+        if (val[0].Value == .01) { // Reject pennies and put it directly to change amount
+            $scope.changeAmount += val[0].Value;
         } else {
-            $scope.currentAmount += val;
+            $scope.currentAmount += val[0].Value;
             $scope.statusMessage = $filter('currency')($scope.currentAmount, "$", 2)
         }
     }
@@ -106,7 +158,7 @@ app.controller('ctrl', function ($scope, $interval, $filter, productFactory) {
 
     //Take the product off the vending machine
     $scope.takeProduct = function () {
-        $scope.selectedProduct = null;
+        $scope.selectedProducts = [];
     }
 
     //Switch display between the text and the current coin amount inserted into the vending machine
@@ -117,5 +169,64 @@ app.controller('ctrl', function ($scope, $interval, $filter, productFactory) {
             $scope.statusMessage = $scope.statusMessage == 'Insert Coins' ? $filter('currency')($scope.currentAmount, "$", 2) : 'Insert Coins'
         }
     }, 3000);
+
+    $scope.setBackground = function (imgUrl) {
+        return "background: url('" + imgUrl + "') center no-repeat; background-size:cover; height:120px;";
+    }
+
+    var coinPush = function (quantity, coin) {
+        for (var i = 0; i < quantity; i++) {
+            $scope.changes.push(angular.copy(coin));
+        }
+    }
+
+    function round(value, decimals) {
+        return Number(Math.round(value + 'e' + decimals) + 'e-' + decimals);
+    }
+    //check if there's change to convert to coins
+    $scope.$watch('changeAmount', function (newVal, oldVal, scope) {
+        //convert to coins
+        if (newVal > 0) {
+            newVal = round(newVal, 2);
+            var total = 0;
+            var quarters = Math.trunc(newVal / .25);
+            total += quarters * .25;
+            var dimes = Math.trunc(round((newVal - total), 2) / .10);
+            total += dimes * .10;
+            var nickels = Math.trunc(round((newVal - total), 2) / .05);
+            total += nickels * .05;
+            var pennies = Math.trunc(round((newVal - total), 2) / .01);
+
+            if (quarters > 0) {
+                var coin = $.grep($scope.coins, function (x) {
+                    return x.WeightInGrams == 5.670
+                })[0];
+                coinPush(quarters, coin)
+            }
+            if (dimes > 0) {
+                var coin = $.grep($scope.coins, function (x) {
+                    return x.WeightInGrams == 2.268
+                })[0];
+                coinPush(dimes, coin)
+            }
+
+            if (nickels > 0) {
+                var coin = $.grep($scope.coins, function (x) {
+                    return x.WeightInGrams == 5
+                })[0];
+                coinPush(nickels, coin)
+            }
+
+            if (pennies > 0) {
+                var coin = $.grep($scope.coins, function (x) {
+                    return x.WeightInGrams == 2.5
+                })[0];
+                coinPush(pennies, coin)
+            }
+        } else {
+            $scope.changes = [];
+        }
+    });
+
 
 });
